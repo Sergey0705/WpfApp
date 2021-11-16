88843c84b742949c1facbc676ed83785ffb5ffcc
Sergey0705 <pavrun85@mail.ru>
2021-11-16 21:21:58 +0600

    Команда как ресурс в разметке

---
+++ WpfApp/Infrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using WpfApp.Infrastructure.Commands.Base;
+
+namespace WpfApp.Infrastructure.Commands
+{
+    internal class CloseApplicationCommand : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object parameter) => Application.Current.Shutdown();
+    }
+}
