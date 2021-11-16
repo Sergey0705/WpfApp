57abbe8b9e0970784b5e1e391dd69af686096cb2
Sergey0705 <pavrun85@mail.ru>
2021-11-16 22:37:52 +0600

    Запрос к удаленному серверу для получения сырых данных

--- Tests/WpfAppConsole/Program.cs
+++ Tests/WpfAppConsole/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
 
 namespace WpfAppConsole
 {
     class Program
     {
+        private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse-covid_19_time_series/time_series_covid19_confirmed_global.csv";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //WebClient client = new WebClient();
+
+            var client = new HttpClient();
+
+            var response = client.GetAsync(data_url).Result;
+            var csv_str = response.Content.ReadAsStringAsync().Result;
+
+            Console.ReadLine();
         }
     }
 }
