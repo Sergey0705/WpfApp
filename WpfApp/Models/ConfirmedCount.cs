400bfbd76dd19cdad94331da6cc62568deb3854d
Sergey0705 <pavrun85@mail.ru>
2021-11-17 19:33:08 +0600

    Добавляем модели

---
+++ WpfApp/Models/ConfirmedCount.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp.Models
+{
+    internal struct ConfirmedCount
+    {
+        public DateTime Data { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ WpfApp/Models/PlaceInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace WpfApp.Models
+{
+    class PlaceInfo
+    {
+        public string Name { get; set; }
+        public Point Location { get; set; }
+        public IEnumerable<ConfirmedCount> Counts { get; set; }
+    }
+}
