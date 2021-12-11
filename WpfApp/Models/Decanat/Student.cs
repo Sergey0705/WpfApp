a038ee2ed386ae2a9c463815d58c41ecb9285ea5
Sergey0705 <pavrun85@mail.ru>
2021-12-11 19:48:56 +0600

    Тестовая предметная область

---
+++ WpfApp/Models/Decanat/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WpfApp.Models.Decanat
+{
+   internal class Student
+   {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public DateTime Burthday { get; set; }
+        public double Rating { get; set; }
+   }
+
+    internal class Group
+    {
+        public string Name { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+    }
+}
