8376611194ba5de98c882608389696d86d5120ce
Sergey0705 <pavrun85@mail.ru>
2022-01-01 00:11:21 +0600

    Базовая логика модели-представления статистики

--- WpfApp/ViewModels/CountriesStatisticViewModel.cs
+++ WpfApp/ViewModels/CountriesStatisticViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using WpfApp.Infrastructure.Commands;
+using WpfApp.Models;
 using WpfApp.Services;
 using WpfApp.ViewModels.Base;
 
@@ -11,11 +14,40 @@ namespace WpfApp.ViewModels
         private DataService _DataService;
         private MainWindowViewModel MainModel { get; }
 
+        #region Countries : IEnumerable<CountryInfo> - Статистика по странам
+
+        private IEnumerable<CountryInfo> _Countries;
+
+        public IEnumerable<CountryInfo> Countries
+        {
+            get => _Countries;
+            private set => Set(ref _Countries, value); 
+        }   
+
+        #endregion
+
+        #region Команды
+
+        public ICommand RefreshDataCommand { get; }
+
+        private void OnRefreshDataCommandExecuted(object p)
+        {
+            Countries = _DataService.GetData();
+        }
+
+        #endregion
+
         public CountriesStatisticViewModel(MainWindowViewModel MainModel)
         {
             this.MainModel = MainModel;
 
             _DataService = new DataService();
+
+            #region Команды
+
+            RefreshDataCommand = new LambdaCommand(OnRefreshDataCommandExecuted);
+
+            #endregion
         }
     }
 }
