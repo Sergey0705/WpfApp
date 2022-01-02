a9d4d69d9b28da6167afe2cca8bda1bcb79312d3
Sergey0705 <pavrun85@mail.ru>
2022-01-02 17:40:07 +0600

    Конвертеры, ресурсы кистей, блокировка Freezable объектов в разметке для повышения быстродействия

---
+++ WpfApp/Infrastructure/Converters/Converter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace WpfApp.Infrastructure.Converters
+{
+    internal abstract class Converter : IValueConverter
+    {
+        public abstract object Convert(object value, Type t, object p, CultureInfo c);
+
+        public virtual object ConvertBack(object value, Type t, object p, CultureInfo c)
+        {
+            throw new NotSupportedException("Обратное преобразование не поддерживается");
+        }
+    }
+}
