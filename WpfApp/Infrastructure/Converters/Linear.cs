001dae64e69099a3460dea193d91c8c42419fff6
Sergey0705 <pavrun85@mail.ru>
2022-01-02 21:07:04 +0600

    Мульти-конвертер

--- WpfApp/Infrastructure/Converters/Linear.cs
+++ WpfApp/Infrastructure/Converters/Linear.cs
@@ -2,18 +2,35 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Text;
+using System.Windows.Data;
+using System.Windows.Markup;
 
 namespace WpfApp.Infrastructure.Converters
 {
+    [ValueConversion(typeof(double), typeof(double))]
     /// <summary>
     /// Реаллизация линейного преобразования f(x) = k*x + b
     /// </summary>
     internal class Linear : Converter
     {
+        [ConstructorArgument("K")]
         public double K { get; set; } = 1;
 
-        public double B { get; set; } 
+        [ConstructorArgument("B")]
+        public double B { get; set; }
 
+        public Linear() { }
+
+        public Linear(double K)
+        {
+            this.K = K;
+        }
+
+        public Linear(double K, double B) : this(K)
+        {
+            this.B = B;
+        }
+  
         public override object Convert(object v, Type t, object p, CultureInfo c)
         {
             if (v is null) return null;
